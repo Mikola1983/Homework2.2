f518c13126b93a177112213e2003f85e82f10fd3
Mikola1983 <Mikolaprigorsk@mail.ru>
2025-11-18 05:26:12 +0700

    Homework 2.2

---
+++ src/org/skypro/skyshop/Main.java
@@ -0,0 +1,9 @@
+package org.skypro.skyshop;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+Product book = new Product("Книга",100);
+    }
+}
---
+++ src/org/skypro/skyshop/Product.java
@@ -0,0 +1,11 @@
+package org.skypro.skyshop;
+
+public class Product {
+    private String title;
+    private int price;
+
+    public Product(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+}
